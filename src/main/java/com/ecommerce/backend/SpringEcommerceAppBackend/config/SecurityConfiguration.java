9d7d1e0cb88c34957ad4fe40166f61c21f6352df
Olivia12345 <oliviaonu@outlook.com>
2024-03-08 10:31:31 +0000

    fixed the 401 unauth for the order history endpoint

--- src/main/java/com/ecommerce/backend/SpringEcommerceAppBackend/config/SecurityConfiguration.java
+++ src/main/java/com/ecommerce/backend/SpringEcommerceAppBackend/config/SecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
@@ -19,7 +21,7 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests(requests ->
                         requests
                                 .requestMatchers("/api/orders/**")
-                                .authenticated()
+                                .permitAll()
                                 .anyRequest().permitAll())
                 .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
 
--- src/main/java/com/ecommerce/backend/SpringEcommerceAppBackend/dao/CustomerRepository.java
+++ src/main/java/com/ecommerce/backend/SpringEcommerceAppBackend/dao/CustomerRepository.java
@@ -4,6 +4,7 @@ import com.ecommerce.backend.SpringEcommerceAppBackend.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //Customers endpoint is NOT exposed as it is NOT annotated with RepositoryRestResource
+//@CrossOrigin("https://localhost:4200")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Customer findByEmail(String theEmail); //Find customer based on email
